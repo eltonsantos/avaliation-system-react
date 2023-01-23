0dd41a3dd780447b2627a66a2ea1cb5c89dcd893
Elton Santos <eltonaxl@hotmail.com>
2023-01-23 02:38:51 -0300

    Create state to user

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,6 +1,20 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 import { Menu } from "../../components/Menu";
+import { auth } from "../../services/firebaseConfig";
 
 export function Admin() {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("Uid: ", uid);
+      } else {
+        console.log("User is logged out");
+      }
+    });
+  }, []);
+
   return (
     <>
       <h1>Admin</h1>
