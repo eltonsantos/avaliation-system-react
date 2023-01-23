321f21b78b474ac250a5d1887cd72121bdebe4fa
Elton Santos <eltonaxl@hotmail.com>
2023-01-23 02:03:34 -0300

    Create login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,24 @@
 export function Login() {
-  return <h1>Login</h1>;
+  function handleLogin() {}
+  return (
+    <>
+      <h1>Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </>
+  );
 }
