238cdaabfbcbfe86e074a7a1f7d2f4a43c0fe284
Elton Santos <eltonaxl@hotmail.com>
2023-01-23 02:46:42 -0300

    Create logout function

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,12 +1,27 @@
-import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../services/firebaseConfig";
 
 export function Menu() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+        console.log("Signed out successfully");
+      })
+      .catch((error) => {
+        console.log(error.mensage);
+      });
+  }
+
   return (
     <ul>
       <li>
         <Link to="/admin">Home</Link>
         <Link to="/collaborator">Add Collaborator</Link>
-        <Link to="/">Logout</Link>
+        <button onClick={handleLogout}>Logout</button>
       </li>
     </ul>
   );
