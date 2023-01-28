8c4a504341ce3c1691fdce270cc3fc0df20be6c8
Elton Santos <eltonaxl@hotmail.com>
2023-01-28 17:07:02 -0300

    Styling thanks page

--- src/pages/Thanks/index.jsx
+++ src/pages/Thanks/index.jsx
@@ -1,10 +1,14 @@
+import imgLogo from "../../assets/small-blue.png";
 import "./styles.css";
 
 export function Thanks() {
   return (
-    <div className="container">
-      <h2>Obrigado pela avaliação</h2>
-      <p>Nossa equipe agradece!</p>
+    <div className="containerThanks">
+      <div className="contentThanks">
+        <img src={imgLogo} alt="Imagem" />
+        <h2>Obrigado pela avaliação</h2>
+        <p>Nossa equipe agradece!</p>
+      </div>
     </div>
   );
 }
