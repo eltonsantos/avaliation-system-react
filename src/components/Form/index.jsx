cbdf516f293a43712ac4d907e293b19c3f7da669
Elton Santos <eltonaxl@hotmail.com>
2023-01-27 21:12:23 -0300

    Rating almost ok

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -6,10 +6,16 @@ import "./styles.css";
 
 export function Form() {
   const [collaborators, setCollaborators] = useState([]);
+  const [name, setName] = useState("");
+  const [service, setService] = useState("");
+  const [answer1, setAnswer1] = useState("");
+  const [answer2, setAnswer2] = useState("");
+  const [answer3, setAnswer3] = useState("");
+  const [answer4, setAnswer4] = useState("");
   const [comment, setComment] = useState("");
 
   const onOptionChangeHandler = (event) => {
-    console.log("User Selected Value - ", event.target.value);
+    console.log("Collaborator Selected Value - ", event.target.value);
   };
 
   const answersCollectionRef = collection(db, "answers");
@@ -30,7 +36,16 @@ export function Form() {
     getCollaborators();
   }, []);
 
-  function handleResult() {
+  async function handleResult() {
+    await addDoc(answersCollectionRef, {
+      name,
+      service,
+      answer1,
+      answer2,
+      answer3,
+      answer4,
+      comment,
+    });
     console.log(currentValue);
     console.log(textareaValue);
   }
@@ -42,7 +57,13 @@ export function Form() {
 
         <div className="form-control">
           <label htmlFor="name">Nome:</label>
-          <input type="text" placeholder="Nome" id="name" />
+          <input
+            type="text"
+            placeholder="Nome"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <i className="fas fa-check-circle"></i>
           <i className="fas fa-exclamation-circle"></i>
           <small>Error message</small>
@@ -50,7 +71,13 @@ export function Form() {
 
         <div className="form-control">
           <label htmlFor="service">Serviço:</label>
-          <input type="text" placeholder="Serviço" id="service" />
+          <input
+            type="text"
+            placeholder="Serviço"
+            id="service"
+            value={service}
+            onChange={(e) => setService(e.target.value)}
+          />
           <i className="fas fa-check-circle"></i>
           <i className="fas fa-exclamation-circle"></i>
           <small>Error message</small>
@@ -72,6 +99,7 @@ export function Form() {
         <div className="stars">
           <h2>A comunicação entre você e o funcionário foi rápida?</h2>
           <Star
+            answer1={answer1}
             onRatingChanged={(newRating) => {
               console.log(
                 `NEW RATING (${newRating}) DETECTED FOR 1.. SAVING TO DB`
@@ -83,6 +111,7 @@ export function Form() {
         <div className="stars">
           <h2>Foi de fácil entendimento?</h2>
           <Star
+            answer2={answer2}
             onRatingChanged={(newRating) => {
               console.log(
                 `NEW RATING (${newRating}) DETECTED FOR 2.. SAVING TO DB`
@@ -94,6 +123,7 @@ export function Form() {
         <div className="stars">
           <h2>Gostou do serviço prestado?</h2>
           <Star
+            answer3={answer3}
             onRatingChanged={(newRating) => {
               console.log(
                 `NEW RATING (${newRating}) DETECTED FOR 3.. SAVING TO DB`
@@ -105,6 +135,7 @@ export function Form() {
         <div className="stars">
           <h2>Recomendaria o serviço para alguém?</h2>
           <Star
+            answer4={answer4}
             onRatingChanged={(newRating) => {
               console.log(
                 `NEW RATING (${newRating}) DETECTED FOR 4.. SAVING TO DB`
@@ -115,10 +146,7 @@ export function Form() {
 
         <div className="stars">
           <h2>Comentário, sugestão ou crítica:</h2>
-
           <textarea
-            name=""
-            id=""
             cols="30"
             rows="10"
             className="textarea"
