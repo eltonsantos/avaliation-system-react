55eaab0fdf8021058f053beb4aace563565dd09a
Elton Santos <eltonaxl@hotmail.com>
2023-01-29 17:07:06 -0300

    Trying resolve routes

--- src/routes/token.jsx
+++ src/routes/token.jsx
@@ -1,7 +1,28 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-export function TokenRoutes() {
-  let auth = { token: true };
+import { db } from "../services/firebaseConfig";
 
-  return auth.token ? <Outlet /> : <Navigate to="/token" />;
+export function TokenRoutes({ id }) {
+  const [token, setToken] = useState("");
+
+  const tokensCollectionRef = collection(db, "tokens");
+
+  useEffect(() => {
+    const getTokens = async () => {
+      const data = await getDocs(tokensCollectionRef);
+      setToken(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    };
+
+    getTokens();
+    console.log("TOKEN: " + id);
+  }, []);
+
+  return token == "undefined" ? <Outlet /> : <Navigate to="/token" />;
 }
