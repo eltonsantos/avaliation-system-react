efeb400b7505f437e3dbfb7f04350a6eb15bc7f7
Elton Santos <eltonaxl@hotmail.com>
2023-02-07 01:42:38 -0300

    Trying create chart in react

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,64 +1,105 @@
 import { useState } from "react";
-import { Cell, Legend, Pie, PieChart, Tooltip } from "recharts";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 
 import { Menu } from "../../components/Menu";
 
 import "./styles.css";
 
 const data = [
-  { name: "Product A", field1: 40, field2: 30 },
-  { name: "Product B", field1: 20, field2: 10 },
-  { name: "Product C", field1: 10, field2: 20 },
-  { name: "Product D", field1: 30, field2: 40 },
+  {
+    id: 1,
+    name: "A comunicação entre você e o funcionário foi rápida?",
+    value: 4,
+    date: "03/10/2022",
+  },
+  { id: 2, name: "Foi de fácil entendimento?", value: 2, date: "02/04/2022" },
+  {
+    id: 3,
+    name: "Gostou do serviço prestado?",
+    value: 3,
+    date: "23/02/2022",
+  },
+  {
+    id: 4,
+    name: "Recomendaria o serviço para alguém?",
+    value: 5,
+    date: "18/06/2022",
+  },
 ];
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const colors = ["#00C49F", "#FFBB28", "#FF8042", "#E84C3D"];
 
 export function Dashboard() {
-  const [filterValue1, setFilterValue1] = useState(0);
-  const [filterValue2, setFilterValue2] = useState(0);
+  const [selectedQuestion, setSelectedQuestion] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
 
-  const filteredData = data.filter(
-    (item) => item.field1 > filterValue1 && item.field2 > filterValue2
-  );
+  const handleQuestionFilter = (event) => {
+    setSelectedQuestion(event.target.value);
+  };
+
+  const handleDateFilter = (event) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const filteredData = data
+    .filter((d) => (selectedQuestion ? d.name === selectedQuestion : true))
+    .filter((d) => (selectedDate ? d.date === selectedDate : true));
 
   return (
     <div className="containerDashboard">
       <h1>Dashboard</h1>
       <Menu />
 
-      <div>
-        Filter 1:
-        <input
-          type="number"
-          value={filterValue1}
-          onChange={(e) => setFilterValue1(e.target.value)}
-        />
-      </div>
-      <div>
-        Filter 2:
-        <input
-          type="number"
-          value={filterValue2}
-          onChange={(e) => setFilterValue2(e.target.value)}
-        />
-      </div>
-      <PieChart width={400} height={400}>
-        <Pie
-          data={filteredData}
-          cx={200}
-          cy={200}
-          innerRadius={60}
-          outerRadius={80}
-          dataKey="field1"
-        >
-          {filteredData.map((entry, index) => (
-            <Cell key={index} fill={COLORS[index % COLORS.length]} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
+      >
+        <select onChange={handleQuestionFilter} value={selectedQuestion}>
+          <option value="">Todas as perguntas</option>
+          {data.map((d) => (
+            <option key={d.id} value={d.name}>
+              {d.name}
+            </option>
           ))}
-        </Pie>
-        <Tooltip />
-        <Legend />
-      </PieChart>
+        </select>
+
+        <select onChange={handleDateFilter} value={selectedDate}>
+          <option value="">Todas as datas</option>
+          {data.map((d) => (
+            <option key={d.id} value={d.date}>
+              {d.date}
+            </option>
+          ))}
+        </select>
+      </div>
+      <ResponsiveContainer height={400}>
+        <PieChart>
+          <Pie
+            data={filteredData}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            label
+          >
+            {filteredData.map((entry, index) => (
+              <Cell key={index} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+          <Legend />
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
     </div>
   );
 }
